05bd1fc9721268248b381298add4558853ce6a71
Iowel <akiko3000akiko@gmail.com>
2025-03-27 08:45:39 -0400

    Create getAccount Handler

--- api/server.go
+++ api/server.go
@@ -17,12 +17,11 @@ func NewServer(store *db.Store) *Server {
 	router := gin.Default()
 
 	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
 
 	server.router = router
 	return server
 }
-
-func Hello()
 
 // Start runs the HTTP server on a specific address
 func (server *Server) Start(address string) error {
