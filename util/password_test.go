31b10328968c477f5613fa8fc5396976f7386c1e
Iowel <akiko3000akiko@gmail.com>
2025-03-27 13:51:02 -0400

    Change password test

--- util/password_test.go
+++ util/password_test.go
@@ -10,14 +10,19 @@ import (
 func TestPassword(t *testing.T) {
 	password := RandomString(6)
 
-	HashedPassword, err := HashPassword(password)
+	HashedPassword1, err := HashPassword(password)
 	require.NoError(t, err)
-	require.NotEmpty(t, HashedPassword)
+	require.NotEmpty(t, HashedPassword1)
 
-	err = CheckPassword(password, HashedPassword)
+	err = CheckPassword(password, HashedPassword1)
 	require.NoError(t, err)
 
 	wrongPassword := RandomString(6)
-	err = CheckPassword(wrongPassword, HashedPassword)
+	err = CheckPassword(wrongPassword, HashedPassword1)
 	require.EqualError(t, err, bcrypt.ErrMismatchedHashAndPassword.Error())
+
+	HashedPassword2, err := HashPassword(password)
+	require.NoError(t, err)
+	require.NotEmpty(t, HashedPassword2)
+	require.NotEqual(t, HashedPassword1, HashedPassword2)
 }
