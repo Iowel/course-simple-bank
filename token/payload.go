5777cf8935b837124359836d54ea7510c227fc79
Iowel <akiko3000akiko@gmail.com>
2025-03-28 02:46:23 -0400

    Create  JWT verify and add testing

---
+++ token/payload.go
@@ -0,0 +1,46 @@
+package token
+
+import (
+	"errors"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+var (
+	ErrExpiredToken = errors.New("token has expired")
+	ErrInvalidToken = errors.New("token is invalid")
+)
+
+// Структура содержащая данные токена
+type Payload struct {
+	ID        uuid.UUID `json:"id"`
+	Username  string    `json:"username"`
+	IssuedAt  time.Time `json:"issued_at"`
+	ExpiredAt time.Time `json:"expired_at"`
+}
+
+// Создает новый токен с определенным юзернеймом и длительностью
+func NewPayload(username string, duration time.Duration) (*Payload, error) {
+	// Генерация уникального ID токена
+	tokenID, err := uuid.NewRandom()
+	if err != nil {
+		return nil, err
+	}
+
+	payload := &Payload{
+		ID:        tokenID,
+		Username:  username,
+		IssuedAt:  time.Now(),
+		ExpiredAt: time.Now().Add(duration),
+	}
+	return payload, nil
+}
+
+// Проверка на истечение срока действия токена
+func (payload *Payload) Valid() error {
+	if time.Now().After(payload.ExpiredAt) {
+		return ErrExpiredToken
+	}
+	return nil
+}
