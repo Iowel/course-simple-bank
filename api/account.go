6b2660ebef7c0a87182072486ec31dec674d37d3
Iowel <akiko3000akiko@gmail.com>
2025-03-28 04:47:38 -0400

    Add Authorization rules in API

--- api/account.go
+++ api/account.go
@@ -2,15 +2,16 @@ package api
 
 import (
 	"database/sql"
+	"errors"
 	"net/http"
 
 	db "github.com/Iowel/course-simple-bank/db/sqlc"
+	"github.com/Iowel/course-simple-bank/token"
 	"github.com/gin-gonic/gin"
 	"github.com/lib/pq"
 )
 
 type createAccountRequest struct {
-	Owner    string `json:"owner" binding:"required"`
 	Currency string `json:"currency" binding:"required,currency"`
 }
 
@@ -21,8 +22,12 @@ func (server *Server) createAccount(ctx *gin.Context) {
 		return
 	}
 
+	// Извлекаем Payload из контекста, authPayload содержит данные токена (например, username, exp и т. д.)
+	//Добавление авторизации в обработчик создания аккаунтa
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
+
 	arg := db.CreateAccountParams{
-		Owner:    req.Owner,
+		Owner:    authPayload.Username,
 		Currency: req.Currency,
 		Balance:  0,
 	}
@@ -64,6 +69,14 @@ func (server *Server) getAccount(ctx *gin.Context) {
 		return
 	}
 
+	// Проверка принадлежит ли account (счёт, ресурс) аутентифицированному пользователю
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
+	if account.Owner != authPayload.Username {
+		err := errors.New("account doesn't belong to the authenticated user")
+		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
+		return
+	}
+
 	ctx.JSON(http.StatusOK, account)
 }
 
@@ -80,7 +93,9 @@ func (server *Server) listAccount(ctx *gin.Context) {
 		return
 	}
 
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
 	arg := db.ListAccountsParams{
+		Owner:  authPayload.Username,
 		Limit:  req.PageSize,
 		Offset: (req.PageID - 1) * req.PageSize,
 	}
--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -90,20 +90,23 @@ func TestDeleteAccount(t *testing.T) {
 }
 
 func TestListAccounts(t *testing.T) {
+	var lsatAccount Account
 	for i := 0; i < 10; i++ {
-		createRandomAccount(t)
+		lsatAccount = createRandomAccount(t)
 	}
 
 	arg := ListAccountsParams{
+		Owner:  lsatAccount.Owner,
 		Limit:  5,
-		Offset: 5,
+		Offset: 0,
 	}
 
 	accounts, err := testQueries.ListAccounts(context.Background(), arg)
 	require.NoError(t, err)
-	require.Len(t, accounts, 5)
+	require.NotEmpty(t, accounts)
 
 	for _, account := range accounts {
 		require.NotEmpty(t, account)
+		require.Equal(t, lsatAccount.Owner, account.Owner)
 	}
 }
