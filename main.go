2ad76c7ea7cd18bab395dcb486fd47bfb1f6b36c
Iowel <akiko3000akiko@gmail.com>
2025-04-02 16:39:34 -0400

    Embed static frontend files inside Golang backend server's binary

--- main.go
+++ main.go
@@ -7,7 +7,9 @@ import (
 	"net"
 	"net/http"
 
+	_ "github.com/Iowel/course-simple-bank/doc/statik"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"github.com/rakyll/statik/fs"
 
 	"github.com/Iowel/course-simple-bank/api"
 	db "github.com/Iowel/course-simple-bank/db/sqlc"
@@ -87,8 +89,12 @@ func runGatewayServer(config util.Config, store db.Store) {
 	mux := http.NewServeMux()
 	mux.Handle("/", grpcMux)
 
-	fs := http.FileServer(http.Dir("./doc/swagger"))
-	mux.Handle("/swagger/", http.StripPrefix("/swagger/", fs))
+	statikFS, err := fs.New()
+	if err != nil {
+		log.Fatal("cannot create statik fs:", err)
+	}
+	swaggerHandler := http.StripPrefix("/swagger/", http.FileServer(statikFS))
+	mux.Handle("/swagger/", swaggerHandler)
 
 	listener, err := net.Listen("tcp", config.HTTPServerAddress)
 	if err != nil {
