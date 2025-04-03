1cee4fd483f2deaf53dc9a6d807af493a67813d0
Iowel <akiko3000akiko@gmail.com>
2025-04-03 09:12:42 -0400

    How to write HTTP logger middleware in Go

--- main.go
+++ main.go
@@ -28,13 +28,13 @@ import (
 )
 
 func main() {
-	
+
 	config, err := util.LoadConfig(".")
 	if err != nil {
 		log.Fatal().Msg("cannot load config:")
 	}
 
-	if config.Environment == "development"{
+	if config.Environment == "development" {
 		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 	}
 
@@ -131,7 +131,9 @@ func runGatewayServer(config util.Config, store db.Store) {
 
 	log.Info().Msgf("start HTTP gateway server at %s", listener.Addr().String())
 
-	err = http.Serve(listener, mux)
+	handler := gapi.HttpLogger(mux)
+
+	err = http.Serve(listener, handler)
 	if err != nil {
 		log.Fatal().Msg("cannot start HTTP server:")
 	}
